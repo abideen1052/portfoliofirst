76b71be92c75af4fe576e9cb8c601f71bfd697f5
Mohammed Zainul Abideen <abideen1052@gmail.com>
2023-04-14 16:55:21 +0530

    Folder structure for basic componentes are created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+import { About,Footer,Header,Skills,Testimonial,Work} from './container'
+import { Navbar } from './components';
 
 function App() {
   return (
-    <h1>
-      App
-    </h1>
+    <div className='app'>
+      <Navbar/>
+    <Header/>
+    <About/>
+    <Work/>
+    <Skills/>
+    <Testimonial/>
+    <Footer/>
+    </div>
   );
 }
 
