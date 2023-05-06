f48a6419fccfb80bfe280bcd788134dde1ffcd3f
Mohammed Zainul Abideen <abideen1052@gmail.com>
2023-05-06 12:50:34 +0530

    Contact section added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.scss';
-import { About,Footer,Header,Skills,Testimonial,Work} from './container'
+import { About,Footer,Header,Skills,Work} from './container'
 import { Navbar } from './components';
 
 function App() {
@@ -10,7 +10,6 @@ function App() {
     <About/>
     <Work/>
     <Skills/>
-    <Testimonial/>
     <Footer/>
     </div>
   );
